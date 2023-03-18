0f4e09f39142d875ca7075c727870cd51caaf710
Matthias Grosperrin <matthias.g@mozzaik365.com>
2023-03-18 21:15:15 +0100

    Create repro

---
+++ ExecuteUpdateOwnedEntity/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var contextOptions = new DbContextOptionsBuilder()
+    .UseInMemoryDatabase("Test").Options;
+
+var context = new EntityContext(contextOptions);
+context.Entities.ExecuteUpdate(
+    updates => updates.SetProperty(e => e.Name, "Test"));
+
+public class Entity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public SubEntity Owned { get; set; }
+}
+public class SubEntity
+{
+    public string Value { get; set; }
+}
+
+class EntityContext : DbContext
+{
+    public EntityContext(DbContextOptions options)
+        : base(options)
+    { }
+
+    public DbSet<Entity> Entities { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        var entityModelBuilder = modelBuilder.Entity<Entity>();
+        entityModelBuilder
+            .HasKey(e => e.Id);
+        entityModelBuilder.OwnsOne(e => e.Owned);
+    }
+}
