3cfc99a16c5be6f81af53275cbe774b14c361263
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2023-09-12 11:36:59 +0200

    Filtered Include & SelectMany fails :(

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 var contextOptions = new DbContextOptionsBuilder()
-    .UseSqlServer("Server=localhost;Database=test;User Id=sa;Password=yourStrong(!)Password;Encrypt=false;MultipleActiveResultSets=true;").Options;
+    .UseSqlServer("Server=localhost;Database=filteredinclude;User Id=sa;Password=yourStrong(!)Password;Encrypt=false;MultipleActiveResultSets=true;").Options;
 
+var widgetId = Guid.NewGuid();
+var dashboardId = Guid.NewGuid();
 var context = new EntityContext(contextOptions);
+var widgets = context.Dashboards
+        .Include(x => x.Widgets.Where(w => w.WidgetId == widgetId))
+        .Where(d => d.DashboardId == dashboardId)
+        .SelectMany(d => d.Widgets);
 
+Console.WriteLine(widgets.ToQueryString());
 
+public  class Dashboard
+{
+    public Guid DashboardId { get; set; }
+    public IEnumerable<Widget> Widgets { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
 
+public class Widget
+{
+    public Guid WidgetId { get; set; }
+    public Guid DashboardId { get; set; }
+    public string Name { get; set; } = null!;
+}
 class EntityContext : DbContext
 {
+    public DbSet<Dashboard> Dashboards { get; set; }
     public EntityContext(DbContextOptions options)
         : base(options)
     { }
@@ -16,5 +37,28 @@ class EntityContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new DashboardMapping())
+                    .ApplyConfiguration(new WidgetMapping())
+                    ;
+    }
+    class DashboardMapping : IEntityTypeConfiguration<Dashboard>
+    {
+        public virtual void Configure(EntityTypeBuilder<Dashboard> builder)
+        {
+            builder.HasKey(e => e.DashboardId);
+            builder.Property(e => e.DashboardId).HasColumnName("DashboardId").IsRequired();
+            builder.HasMany(e => e.Widgets).WithOne().HasForeignKey(w => w.DashboardId);
+            builder.Property(e => e.Name).HasColumnName("Name").IsRequired();
+        }
+    }
+    class WidgetMapping : IEntityTypeConfiguration<Widget>
+    {
+        public virtual void Configure(EntityTypeBuilder<Widget> builder)
+        {
+            builder.HasKey(x => x.WidgetId);
+            builder.Property(x => x.WidgetId).HasColumnName("WidgetId");
+            builder.Property(x => x.DashboardId).HasColumnName("DashboardId");
+            builder.Property(e => e.Name).HasColumnName("Name").IsRequired();
+        }
     }
 }
