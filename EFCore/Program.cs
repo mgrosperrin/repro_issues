35b242450b8684f2fc32d9cf6a01da4dac97e169
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2023-09-12 11:06:28 +0200

    Setup EFCore

---
+++ EFCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var contextOptions = new DbContextOptionsBuilder()
+    .UseSqlServer("Server=localhost;Database=test;User Id=sa;Password=yourStrong(!)Password;Encrypt=false;MultipleActiveResultSets=true;").Options;
+
+var context = new EntityContext(contextOptions);
+
+
+
+class EntityContext : DbContext
+{
+    public EntityContext(DbContextOptions options)
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
